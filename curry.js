81b92c7f32c573b03d33e1a1af3ea2b884a8c439
qibin <@qibin@smart-haier.com>
2017-03-21 01:04:21 +0800

    upload project

---
+++ curry.js
@@ -0,0 +1,18 @@
+var curry = function(fn, length) {
+	length = length || fn.length;
+	return function() {
+		if (arguments.length < length) {
+			var combined = [fn].concat(Array.prototype.slice.call(arguments, 0));
+			return curry(curry_once.apply(this, combined), length - arguments.length);
+		} else {
+			return fn.apply(this, arguments);
+		}
+	};
+}
+
+var curry_once = function(fn) {
+	var args = Array.prototype.slice.call(arguments, 1);
+	return function() {
+		return fn.apply(this, args.concat(Array.prototype.slice.call(arguments, 0)));
+	};
+};
